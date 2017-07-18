e6ecc68091487d904b39ddfd2c0dfe9b94315b32
y0711 <huangyabin8@gmail.com>
2017-07-17 21:24:44 -0700

    Create AbstractFactory.java

---
+++ AbstractFactory.java
@@ -0,0 +1,77 @@
+import static java.lang.System.out;
+
+
+interface Button {
+	void show();
+}
+
+class BigButton implements Button {
+	@Override
+	public show() {
+		out.println("show big button");
+	}
+}
+
+class SmallButton implements Button {
+	@Override
+	public void show() {
+		out.println("show small button");
+	}
+}
+
+interface Text {
+	void show();
+}
+
+class LongText implements Text {
+	@Override
+	public void show() {
+		out.println("show long text");
+	}
+}
+
+class ShortText implements Text {
+	@Override
+	public void show() {
+		out.println("show short text");
+	}
+}
+
+interface Factory {
+	Button getButton();
+	Text getText();
+}
+
+class BigFactory implements Factory {
+	@Override
+	public Button getButton() {
+		return new BigButton();
+	}
+	@Override
+	public Text getText() {
+		return new LongText();
+	}
+}
+
+class SmallFactory implements Factory {
+	@Override
+	public Button getButton() {
+		return new SmallButton();
+	}
+	@Override
+	public Text getText() {
+		return new ShortText();
+	}
+}
+
+public class AbstractFactory {
+
+	public static void main(String[] args) {
+		Factory f = new SmallFactory();
+		Button b = f.getButton();
+		Text t = f.getText();
+		b.show();
+		t.show();
+	}
+
+}
